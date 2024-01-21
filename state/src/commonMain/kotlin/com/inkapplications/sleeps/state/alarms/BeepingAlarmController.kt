bf6c862d43dbaeae37d6c035953a71d017c15ff5
Renee Vandervelde <Renee@ReneeVandervelde.com>
2024-01-21 14:31:01 -0600

    Accelerate alarm beeps slowly

--- state/src/commonMain/kotlin/com/inkapplications/sleeps/state/alarms/BeepingAlarmController.kt
+++ state/src/commonMain/kotlin/com/inkapplications/sleeps/state/alarms/BeepingAlarmController.kt
@@ -3,15 +3,22 @@ package com.inkapplications.sleeps.state.alarms
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
+import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 internal class BeepingAlarmController(
     private val beeper: AlarmBeeper,
 ): AlarmController {
+    private val initialDelay = 1.minutes
+    private val minimumDelay = 3.seconds
+    private val acceleration = 0.9
+
     override suspend fun onStartAlarm(id: AlarmId) {
+        var current = initialDelay
         while (currentCoroutineContext().isActive) {
             beeper.play()
-            delay(3.seconds)
+            delay(current)
+            current = (current * acceleration).coerceAtLeast(minimumDelay)
         }
     }
 }
